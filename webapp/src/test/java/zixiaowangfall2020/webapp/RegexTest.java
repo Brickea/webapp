439cfd3fe287596576e813bff69f5e4cc39e829a
ZixiaoWang <wang.zixi@northeastern.edu>
2020-10-02 11:45:28 -0300

    unit test finish

--- webapp/src/test/java/zixiaowangfall2020/webapp/RegexTest.java
+++ webapp/src/test/java/zixiaowangfall2020/webapp/RegexTest.java
@@ -1,6 +1,7 @@
 package zixiaowangfall2020.webapp;
 
 import org.junit.jupiter.api.Test;
+import zixiaowangfall2020.webapp.util.RegexHelper;
 
 /**
  * @Author: Zixiao Wang
@@ -10,8 +11,39 @@ import org.junit.jupiter.api.Test;
 
 public class RegexTest {
 
-//    @Test
-//    public passwordStrengthCheckTest(){
-//        assert
-//    }
+    @Test
+    public void betterThanNullTest(){
+        String password = "fjw";
+        assert ((Integer)RegexHelper.checkPassordStrength(password).values().toArray()[0])==-1;
+    }
+
+    @Test
+    public void lowercaseTest(){
+        String password = "fjwwzxqweasd";
+        assert ((Integer)RegexHelper.checkPassordStrength(password).values().toArray()[0])==1;
+    }
+
+    @Test
+    public void uppercaseTest(){
+        String password = "WZXQWEASDZXC";
+        assert ((Integer)RegexHelper.checkPassordStrength(password).values().toArray()[0])==1;
+    }
+
+    @Test
+    public void lowerAndUppercaseTest(){
+        String password = "fjwwzxqwSDFeasd";
+        assert ((Integer)RegexHelper.checkPassordStrength(password).values().toArray()[0])==2;
+    }
+
+    @Test
+    public void numberTest(){
+        String password = "123456789";
+        assert ((Integer)RegexHelper.checkPassordStrength(password).values().toArray()[0])==1;
+    }
+
+    @Test
+    public void complexTest(){
+        String password = "fjwWZX970814";
+        assert ((Integer)RegexHelper.checkPassordStrength(password).values().toArray()[0])==3;
+    }
 }
