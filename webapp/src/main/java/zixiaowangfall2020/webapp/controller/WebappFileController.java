112d62ca404b0c8693a2c4b229bb387ad86180b2
ZixiaoWang <wang.zixi@northeastern.edu>
2020-11-03 17:43:12 -0500

    webapp upload test

--- webapp/src/main/java/zixiaowangfall2020/webapp/controller/WebappFileController.java
+++ webapp/src/main/java/zixiaowangfall2020/webapp/controller/WebappFileController.java
@@ -1,6 +1,14 @@
 package zixiaowangfall2020.webapp.controller;
 
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.Bucket;
+import com.amazonaws.services.s3.model.ListObjectsRequest;
+import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -8,6 +16,10 @@ import org.springframework.web.multipart.MultipartFile;
 import zixiaowangfall2020.webapp.mapper.WebappFileMapper;
 import zixiaowangfall2020.webapp.pojo.WebappFile;
 import zixiaowangfall2020.webapp.service.WebappFileService;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @Author: Zixiao Wang
@@ -23,6 +35,9 @@ public class WebappFileController {
 
     @Autowired
     private WebappFileMapper webappFileMapper;
+
+    @Value("${aws.s3.region}")
+    private String region;
 
     @PostMapping(value= "/upload")
     public ResponseEntity<String> uploadFile(@RequestPart(value= "file") final MultipartFile multipartFile) {
@@ -43,6 +58,40 @@ public class WebappFileController {
         service.deleteQuestionFile(webappFile);
         final String response = "[" + webappFile.getS3ObjectName() + "] delete successfully.";
         return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "test/bucket/list")
+    public ResponseEntity<Map> testListBucket(){
+        Map<String,Object> res = new HashMap<>();
+        final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(region).build();
+        List<Bucket> buckets = s3.listBuckets();
+        for (Bucket b : buckets) {
+            res.put("bucketName",b.getName());
+        }
+
+        return new ResponseEntity<>(res,HttpStatus.OK);
+    }
+
+    @GetMapping(value = "test/bucketObject/list")
+    public ResponseEntity<Map> testListBucketObject(){
+        Map<String,Object> res = new HashMap<>();
+        final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(region).build();
+        ListObjectsRequest listObjectsRequest = new ListObjectsRequest()
+                .withBucketName("webapp.zixiao.wang");
+
+        ObjectListing objectListing;
+
+        do {
+            objectListing = s3.listObjects(listObjectsRequest);
+            for (S3ObjectSummary objectSummary :
+                    objectListing.getObjectSummaries()) {
+                res.put("key",objectSummary.getKey());
+                res.put("size",objectSummary.getSize());
+            }
+            listObjectsRequest.setMarker(objectListing.getNextMarker());
+        } while (objectListing.isTruncated());
+
+        return new ResponseEntity<>(res,HttpStatus.OK);
     }
 
 }
