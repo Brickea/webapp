560b709df95a5be33cc578daa36a5477d497fe73
ZixiaoWang <wang.zixi@northeastern.edu>
2020-11-04 21:07:44 -0500

    update

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -4,4 +4,4 @@ echo "Excute user data"
 sudo /var/lib/cloud/instance/scripts/part-001
 
 echo "Start springboot"
-# java -jar webapp-0.0.1-SNAPSHOT.jar -Dspring.config.location=/home/ubuntu/application.properties
+nohup java -jar -Dspring.config.location=application.properties webapp/webapp-0.0.1-SNAPSHOT.jar &
