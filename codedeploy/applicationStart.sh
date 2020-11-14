a24c5463387515ae097644c8ddc75d4cf4d60693
ZixiaoWang <wang.zixi@northeastern.edu>
2020-11-14 14:47:07 -0500

    fix auth error

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -4,4 +4,4 @@ echo "Excute user data"
 sudo /var/lib/cloud/instance/scripts/part-001
 
 echo "Start springboot"
-sudo nohup java -jar -Dspring.config.location=application.properties webapp/webapp-0.0.1-SNAPSHOT.jar &
+# sudo nohup java -jar -Dspring.config.location=application.properties webapp/webapp-0.0.1-SNAPSHOT.jar &
