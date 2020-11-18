dd30ee24b6a1912e00b35a2a72949091ad92412e
ZixiaoWang <wang.zixi@northeastern.edu>
2020-11-18 09:20:08 -0500

    fix error

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -5,3 +5,8 @@
 
 echo "Start springboot "
 sudo nohup java -jar -Dspring.config.location=/home/ubuntu/application.properties /home/ubuntu/webapp/webapp-0.0.1-SNAPSHOT.jar >/dev/null 2>&1 &
+
+echo "Start cloud watch"
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:/home/ubuntu/infrastructure.json
+
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:/home/ubuntu/app.json
--- codedeploy/beforeInstall.sh
+++ codedeploy/beforeInstall.sh
@@ -3,3 +3,54 @@
 echo "******************Start deploy code******************"
 echo "Excute user data"
 sudo /var/lib/cloud/instance/scripts/part-001
+
+echo "creating cloudwatch configuration file"
+echo '{
+  "logs": {
+    "logs_collected": {
+      "files": {
+        "collect_list": [
+          {
+            "file_path": "/opt/codedeploy-agent/webapp.log",
+            "log_group_name": "webapp"
+          }
+        ]
+      }
+    }
+  }
+}
+' > /home/ubuntu/app.json
+
+echo '{
+  "metrics": {
+    "metrics_collected": {
+      "cpu": {
+        "resources": [
+          "*"
+        ],
+        "measurement": [
+          "usage_active"
+        ],
+        "totalcpu": true
+      },
+      "mem": {
+         "measurement": [
+           "used_percent"
+        ]
+      }
+}
+  },
+  "logs": {
+    "logs_collected": {
+      "files": {
+        "collect_list": [
+          {
+            "file_path": "/opt/aws/amazon-cloudwatch-agent/logs/amazon-cloudwatch-agent.log",
+            "log_group_name": "amazon-cloudwatch-agent"
+          }
+        ]
+      }
+    }
+  }
+}
+' > /home/ubuntu/infrastructure.json
