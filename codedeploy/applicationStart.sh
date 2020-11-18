33d675247a6a58c9a24d6ad9c66ddb789c5184d5
ZixiaoWang <wang.zixi@northeastern.edu>
2020-11-17 21:41:04 -0500

    fix error

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -3,5 +3,5 @@
 # echo "Excute user data"
 # sudo /var/lib/cloud/instance/scripts/part-001
 
-echo "Start springboo "
+echo "Start springboot "
 sudo nohup java -jar -Dspring.config.location=/home/ubuntu/application.properties /home/ubuntu/webapp/webapp-0.0.1-SNAPSHOT.jar >/dev/null 2>&1 &
